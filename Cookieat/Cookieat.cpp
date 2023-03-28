6988a6bbf9e90df1de9ef4a1b3362fa71784c583
Akos Poleczki <a.poleczki@gmail.com>
2023-03-28 15:55:55 +0200

    exception handling added for new items

--- Cookieat/Cookieat.cpp
+++ Cookieat/Cookieat.cpp
@@ -25,7 +25,14 @@ int main() {
         auto random_row_number = real_dist(randomNumberGenerator);
 
         // Check recency sheet and generate acceptance based on it
-        auto recency_value = ws_recency.cell(random_row_number, 1).value().get<int>();
+        int recency_value = 0; // Set default value to zero
+        try {
+            recency_value = ws_recency.cell(random_row_number, 1).value().get<int>();
+        }
+        catch (const OpenXLSX::XLValueTypeError&) {
+            // Cell does not contain a valid value, set to default value
+        }
+
         std::uniform_int<> real_dist2(1, 100);
 
         if (recency_value < real_dist2(randomNumberGenerator)) {
@@ -56,7 +63,13 @@ int main() {
     // Update recencies
     auto rng_recency = ws_recency.range(OpenXLSX::XLCellReference("A2"), OpenXLSX::XLCellReference(rowCount, 1));
     for (auto& cell : rng_recency) {
-        cell.value().set(max(cell.value().get<int>() - 5, 0));
+        try {
+            cell.value().set(max(cell.value().get<int>() - 5, 0));
+        }
+        catch (const OpenXLSX::XLValueTypeError&) {
+            // Cell does not contain a valid value, set to default value
+            cell.value().set(0);
+        }
     }
 
     // Save and close the document.
