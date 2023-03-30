7bba4d7ae609c83f69a125257f38527e8c62e665
Akos Poleczki <a.poleczki@gmail.com>
2023-03-30 23:00:04 +0200

    deleted some comments

--- Cookieat/Cookieat.h
+++ Cookieat/Cookieat.h
@@ -1,7 +1,4 @@
-﻿// Cookieat.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 #include <OpenXLSX.hpp>
@@ -10,5 +7,4 @@
 #include <random>
 #include <windows.h>
 
-// TODO: Reference additional headers your program requires here.
 inline std::mt19937 randomNumberGenerator(std::random_device{}());
